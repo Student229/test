bd7e9d72da07e2e753da9da2e33b9ca508ef3c4c
Student229 <ravijosyula100620072@gmail.com>
2022-04-03 14:39:10 +0500

    gameOver

--- js/Player.js
+++ js/Player.js
@@ -66,4 +66,16 @@ class Player {
       allPlayers = data.val();
     });
   }
+
+  getCarsAtEnd() {
+    database.ref("carsAtEnd").on("value", data => {
+      this.rank = data.val();
+    });
+  }
+
+  static updateCarsAtEnd(rank) {
+    database.ref("/").update({
+      carsAtEnd: rank
+    });
+  }
 }
